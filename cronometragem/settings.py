bab3633fca4c3b04767e602315c778438ad37a0b
CarlosHeiden <carlos.heiden@outlook.com>
2023-06-05 15:34:26 -0300

    acessar django  outro pc , refresch resultados

--- cronometragem/settings.py
+++ cronometragem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f8sb9l!s&!cq(r+z3^ub*oakr+wu*xgh$b&k%k97infegwuu6j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.129', 'localhost', '127.0.0.1']
 
 
 # Application definition
